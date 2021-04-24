20bb94c5b766a2017a937d1d6924e924155b8398
mau2662 <vargas7384@gmail.com>
2021-04-24 20:22:46 +0000

    casi listo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState(null);
+	console.log(color);
+	let redClass = "";
+	if (setColor === "red") {
+		redClass = `selected`;
+	}
+	let yellowClass = "";
+	if (setColor === "yellow") yellowClass = `selected`;
+	let greenClass = "";
+	if (setColor === "green") greenClass = `selected`;
 	return (
 		<div>
 			<div id="traficTop"> </div>
 			<div id="container">
-				<div className="red light"> </div>
-				<div className="yellow light"> </div>
-				<div className="green light"> </div>
+				<div
+					className={"red light " + redClass}
+					onClick={() => {
+						setColor("red");
+					}}>
+					{" "}
+				</div>
+				<div
+					className={"yellow light " + yellowClass}
+					onClick={() => {
+						setColor("yellow");
+					}}>
+					{" "}
+				</div>
+				<div
+					className={"green light " + greenClass}
+					onClick={() => {
+						setColor("green");
+					}}>
+					{" "}
+				</div>
 			</div>
 		</div>
 	);
